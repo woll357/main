73d96b38e531981ca10526180002fed4624e4597
User <User@DESKTOP-5RP96QJ>
2019-05-15 09:55:42 +0900

    20190515 ㅈㅈ

--- src/aacommon_p/Common.java
+++ src/aacommon_p/Common.java
@@ -2,8 +2,8 @@ package aacommon_p;
 
 public class Common {
 	
-	//String 	path = "C:\\apache-tomcat-8.5.38\\webapps\\mainProj\\img";
-	String 	path = "D:\\mainWork\\mainProj\\WebContent\\img";
+	String 	path = "C:\\apache-tomcat-8.5.38\\webapps\\mainProj\\img";
+	//String 	path = "D:\\mainWork\\mainProj\\WebContent\\img";
 	public String getPath() {
 		return path;
 	}
